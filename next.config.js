ef3f2c7e431f96e76893239a06f36298a7ffabd5
Sadath Syed <sadathsadu2002@gmail.com>
2022-08-08 19:08:51 +0530

    next-pw

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
+const withPwa = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPwa({
+  pwa:{
+    register: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest\.json$/],
+    scope: '/',
+    sw: 'service-worker.js',
+    skipWaiting: true,
+    // disable: process.env.NODE_ENV === 'development',
+  },
   reactStrictMode: true,
   swcMinify: true,
   images:{
     domains:["i.ibb.co","i.postimg.cc"]
-  }
-}
+  },
+})
 
 module.exports = nextConfig
