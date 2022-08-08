2adbf53cefe843a70bfd308830155c49826195e0
Sadath Syed <sadathsadu2002@gmail.com>
2022-08-08 19:56:30 +0530

    sw

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import { useEffect } from 'react';
+import DefaultSeo from '../components/DefaultSeo';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+
+  useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
+
 
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
@@ -10,7 +28,10 @@ function MyApp({ Component, pageProps }) {
     }
   }, []);
 
-  return <Component {...pageProps} />
+  return <>
+  <DefaultSeo/>
+   <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
